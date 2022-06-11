d01e762f145a7c040cde25c28bd8ca36d30ebc1e
waynemorphic <wayneklyde@gmail.com>
2022-06-11 13:13:27 +0300

    index function rendering index template

--- core/views.py
+++ core/views.py
@@ -6,3 +6,7 @@ from django.contrib.auth.decorators import login_required
 def sign_out(request):
     logout(request)
     return render(request, 'home/index.html')
+
+# index template function
+def index(request):
+    return render(request, 'home/index.html')
