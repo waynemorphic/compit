a5cffa253533dcf9f58ddd6638437d4daabb171d
waynemorphic <wayneklyde@gmail.com>
2022-06-11 12:27:08 +0300

    logout function

--- core/views.py
+++ core/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.contrib.auth import logout
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+def sign_out(request):
+    logout(request)
+    return render(request, 'home/index.html')
