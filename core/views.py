264ced8172dc3124f82bd5d22a6c3f5535c76a9e
waynemorphic <wayneklyde@gmail.com>
2022-06-12 20:39:04 +0300

    sign out template rendering

--- core/views.py
+++ core/views.py
@@ -3,8 +3,11 @@ from django.contrib.auth import logout
 from django.contrib.auth.decorators import login_required
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from core import serializer
 from core.models import Profile, Projects
 from core.serializer import ProfileSerializer,  ProjectsSerializer
+from rest_framework import status
+from core.permissions import IsAdminOrReadOnly
 
 # blank=True can be used with fields having null=False, 
 # but this will require implementing clean() on the model in order to programmatically supply any missing values.
@@ -12,10 +15,10 @@ from core.serializer import ProfileSerializer,  ProjectsSerializer
 # Create your views here.
 def sign_out(request):
     logout(request)
-    return render(request, 'home/index.html')
+    return render(request, 'registration/login.html')
 
 # index template function
-def index(request):
+def home(request):
     return render(request, 'home/index.html')
 
 # Api endpoints
@@ -23,10 +26,21 @@ class ProfileList(APIView):
     def get(self, request, format = None):
         all_profiles = Projects.objects.all()
         serializers = ProfileSerializer(all_profiles, many = True)
+        permission_classes = (IsAdminOrReadOnly)
         return Response(serializers.data)
+    
+    def post(self, request, format = None):
+        serializers = ProfileSerializer(data = request.data)
+        permission_classes = (IsAdminOrReadOnly)
+        if serializers.is_valid():
+            serializers.save()
+            return Response(serializers.data, status = status.HTTP_201_CREATED)
+        return Response(serializers.errors, status = status.HTTP_400_BAD_REQUEST)
+    
 
 class ProjectsList(APIView):
     def get(self, request, format = None):
         all_projects = Projects.objects.all()
         serializers = ProjectsSerializer(all_projects, many = True)
+        permission_classes = (IsAdminOrReadOnly)
         return Response(serializers.data)
