97907e5e77a625586ef2bae82ac1458e46528850
waynemorphic <wayneklyde@gmail.com>
2022-06-13 18:43:48 +0300

    connecting the signal module to bypass the ready function

--- core/apps.py
+++ core/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class CoreConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'core'
+
+# connecting the signal module that bypasses the ready method
+def ready(self):
+    import core.signals
