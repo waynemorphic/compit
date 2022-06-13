02a5a202c313b3f1cce2a31a0e3edc9c4c199452
waynemorphic <wayneklyde@gmail.com>
2022-06-13 18:43:08 +0300

    users profile url

--- core/urls.py
+++ core/urls.py
@@ -8,11 +8,13 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('', views.home, name = 'home/'),
     path('home/', views.home, name = 'home/'),
+    path('home/post/', views.post_project, name = 'add post'),
     path('accounts/register/home/', views.home, name = 'home/'),
     path('accounts/login/', auth_views.LoginView.as_view(next_page = 'home/'), name = 'login'),
     path('accounts/login/home/', views.home, name = 'home/'),
     path('api/profiles/', views.ProfileList.as_view()),
     path('api/projects/', views.ProjectsList.as_view()),
+    path('home/profile/', views.profile, name = 'user profile'),
 ]
 
 if settings.DEBUG:
