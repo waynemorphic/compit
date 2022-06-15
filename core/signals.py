6c5235a864954edb11fa6aab0333de90ddadc21e
waynemorphic <wayneklyde@gmail.com>
2022-06-15 15:01:08 +0300

    signals to create user profile once new user is registered

--- core/signals.py
+++ core/signals.py
@@ -17,8 +17,10 @@ def create_profile(sender, instance, created, **kwargs):
         Profile.objects.create(user = instance)
     if not created:
         Profile.objects.create(user = instance)
-post_save.connect(create_profile, sender = User)        
-
+        print(instance)
+ 
 @receiver(post_save, sender = User)
 def save_profile(sender, instance, **kwargs):
     instance.profile.save()
+
+post_save.connect(create_profile, sender = User)        
