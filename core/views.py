9a460c091022d36ce6824952fa8c49751d8d6428
waynemorphic <wayneklyde@gmail.com>
2022-06-15 15:03:15 +0300

    profile function refractoring and debug to display single profile after update

--- core/views.py
+++ core/views.py
@@ -9,7 +9,7 @@ from core.serializer import ProfileSerializer,  ProjectsSerializer
 from rest_framework import status
 from core.permissions import IsAdminOrReadOnly
 from django.contrib.auth.models import User
-from core.forms import ProjectForm, ProfileForm, UpdateProfileForm, UpdateUserForm
+from core.forms import ProjectForm, ProfileForm, UpdateProfileForm,  DesignForm, UserLogin
 from django.contrib import messages
 from django.core.exceptions import ObjectDoesNotExist
 
@@ -19,7 +19,8 @@ from django.core.exceptions import ObjectDoesNotExist
 # Create your views here.
 def sign_out(request):
     logout(request)
-    return render(request, 'registration/login.html')
+    form = UserLogin()
+    return render(request, 'registration/login.html', {"form": form})
 
 # index template function
 def home(request):
@@ -29,7 +30,9 @@ def home(request):
     print(users)
     profiles = Profile.objects.all()
     print(profiles)
-    context = {"projects": projects, "users": users, "profiles": profiles}
+    design_form = DesignForm()
+    
+    context = {"projects": projects, "users": users, "profiles": profiles, "design_form": design_form}
     return render(request, 'home/index.html', context)
 
 # post a project
@@ -50,42 +53,112 @@ def post_project(request):
 # user profile
 @login_required
 def profile(request):
-    if request.method == 'POST':
-        user_form = UpdateUserForm(request.POST, instance = request.user)
-        profile_form = UpdateProfileForm(request.POST, request.FILES)
+    current_user = request.user
+#     print(logged_in_user)
+    try: 
+        logged_in_user = Profile.objects.get(user = current_user)
+        if request.method == 'POST':
+            # user_form = UpdateUserForm(request.POST, instance = request.user)
+            profile_form = UpdateProfileForm(request.POST, request.FILES)
         
-        if user_form.is_valid() and profile_form.is_valid():
-            # profile update form 
-            profile_picture = profile_form.save(commit = False)
-            profile_picture = profile_form.cleaned_data['profile_picture']
-            bio = profile_form.cleaned_data['bio']
-            contact = profile_form.cleaned_data['contact']
-            profile_form.bio = bio
-            profile_form.profile_picture = profile_picture
-            profile_form.contact = contact
-            # user update form 
-            username = user_form.cleaned_data['username']
-            email = user_form.cleaned_data['email']
-            user_form.username = username
-            user_form.email = email 
-            # saving the forms data in DB
-            user_form.save()
-            profile_form.save()
-            messages.success(request, "Your profile has been updated successfully")
-            return redirect(to = 'user profile')
+            if profile_form.is_valid():
+                # profile update form 
+                profile_picture = profile_form.save(commit = False)
+                logged_in_user.profile_picture = profile_form.cleaned_data['profile_picture']
+                logged_in_user.bio = profile_form.cleaned_data['bio']
+                logged_in_user.contact = profile_form.cleaned_data['contact']
+                # profile_form.bio = bio
+                # profile_form.profile_picture = profile_picture
+                # profile_form.contact = contact
+                # user update form 
+                # username = user_form.cleaned_data['username']
+                # email = user_form.cleaned_data['email']
+                # user_form.username = username
+                # user_form.email = email 
+                # saving the forms data in DB
+                # user_form.save()
+                # profile_form.save()
+                logged_in_user.save()
+                print(logged_in_user)
+                
+                messages.success(request, "Your profile has been updated successfully")
+                return redirect(to = 'user profile')
+            
+        # user_form = UpdateUserForm(instance = request.user)
+        profile_form = UpdateProfileForm(instance = request.user.profile)
+        
+        # if the profile does not exist, user to fill in their details
+    except ObjectDoesNotExist:      
+        logged = User.objects.get(id = current_user.id)      
+        # user_form = UpdateUserForm()
+        profile_form = UpdateProfileForm()
+        # we call the updateprofile form with a new variable form
+        if request.method == 'POST':
+            form = UpdateProfileForm(request.POST, request.FILES)
+            print(form.data)
+            if form.is_valid():
+                profile = form.save(commit = False)
+                profile.user = logged
+                print(profile)
+                profile.save()
+                messages.success(request, "Your profile has been updated successfully")
+                return redirect(to = 'user profile')
+                
+                       
+# try:
+#        
+#     except
+#     print(request.user.id)
+    
+#    
+    
+    # if request.method == 'POST':
+    #     user_form = UpdateUserForm(request.POST, instance = request.user)
+    #     profile_form = UpdateProfileForm(request.POST, request.FILES)
+        
+    #     if user_form.is_valid() and profile_form.is_valid():
+    #         # profile update form 
+    #         profile_picture = profile_form.save(commit = False)
+    #         logged_in_user.profile_picture = profile_form.cleaned_data['profile_picture']
+    #         logged_in_user.bio = profile_form.cleaned_data['bio']
+    #         logged_in_user.contact = profile_form.cleaned_data['contact']
+    #         # profile_form.bio = bio
+    #         # profile_form.profile_picture = profile_picture
+    #         # profile_form.contact = contact
+    #         # user update form 
+    #         username = user_form.cleaned_data['username']
+    #         email = user_form.cleaned_data['email']
+    #         user_form.username = username
+    #         user_form.email = email 
+    #         # saving the forms data in DB
+    #         # user_form.save()
+    #         # profile_form.save()
+    #         logged_in_user.save()
+    #         print(logged_in_user)
+            
+    #         messages.success(request, "Your profile has been updated successfully")
+    #         return redirect(to = 'user profile')
         # catching exceptions if user profile does not exist
-    else:
-        try:
-            user_form = UpdateUserForm(instance = request.user)
-            profile_form = UpdateProfileForm(instance = request.user.profile)
-        except ObjectDoesNotExist:            
-            user_form = UpdateUserForm()
-            profile_form = UpdateProfileForm()
+        
+        # elif Profile.objects.get(user = current_user) == ObjectDoesNotExist:
+        #     user_form = UpdateProfileForm()
+        #     profile_form = UpdateUserForm()
+        
+        # else:
+        #     user_form = UpdateUserForm()
+        #     profile_form = UpdateProfileForm()
+    # else:
+    #     try:
+    #         user_form = UpdateUserForm(instance = request.user)
+    #         profile_form = UpdateProfileForm(instance = request.user.profile)
+    #     except ObjectDoesNotExist:            
+    #         user_form = UpdateUserForm()
+    #         profile_form = UpdateProfileForm()
     
     profiles = Profile.objects.all() 
     all_projects = Projects.objects.all()
-    print(profiles)
-    context = {"user_form": user_form, "profile_form": profile_form, "profiles": profiles, "all_projects": all_projects}
+    # print(profiles)
+    context = {"profile_form": profile_form, "profiles": profiles, "all_projects": all_projects}
     return render(request, 'users/profile.html', context)
 
 # search projects by name
@@ -123,3 +196,8 @@ class ProjectsList(APIView):
         serializers = ProjectsSerializer(all_projects, many = True)
         permission_classes = (IsAdminOrReadOnly)
         return Response(serializers.data)
+    
+def review(request)    :
+    design_form = DesignForm()
+    context = {"design_form": design_form}
+    return render(request, 'home/index.html', context)
