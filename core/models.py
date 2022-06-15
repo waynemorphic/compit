67310012b68db875cf3e1b519f8d019acfbbb1d6
waynemorphic <wayneklyde@gmail.com>
2022-06-15 15:00:33 +0300

    model properties alterations in terms of relationships

--- core/models.py
+++ core/models.py
@@ -2,18 +2,37 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.    
+
+class Profile(models.Model):
+    '''
+    Args:
+        username, bio, profile_picture, contact
+    '''
+    
+    username = models.CharField(max_length = 251 )
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null= True)
+    bio = models.TextField()
+    profile_picture = models.ImageField(default = 'default.jpg', upload_to = 'media/')
+    contact = models.CharField(max_length=251, null = True)
+    
+    def __str__(self):
+        return self.username
+    
+    def save_profile(self):
+        self.save()
 # projects model
+
 class Projects(models.Model):
     '''
     Args:
-        title, description, image, editor, link, upload
+        title, description, image, editor, link, upload, profile
     '''
     
-    title = models.CharField(max_length = 250, null = False)
-    description = models.CharField(max_length= 250, null = False)
+    title = models.CharField(max_length = 251, null = False)
+    description = models.CharField(max_length= 251, null = False)
     image = models.ImageField(upload_to = 'media/', null = False)
     editor = models.ForeignKey(User, on_delete = models.CASCADE)
-    link = models.CharField(max_length= 250, null = False)
+    link = models.CharField(max_length= 251, null = False)
     upload = models.FileField(upload_to='media/')
     
     def __str__(self):
@@ -26,54 +45,40 @@ class Projects(models.Model):
     def search_projects(cls, search_term):
         find_project = cls.objects.filter(title__icontains = search_term)
         return find_project
-    
-class Profile(models.Model):
-    '''
-    Args:
-        username, bio, profile_picture, projects, contact
-    '''
-    
-    username = models.CharField(max_length = 250 )
-    user = models.OneToOneField(User, on_delete=models.CASCADE, null= True, default = '')
-    bio = models.TextField()
-    profile_picture = models.ImageField(default = 'default.jpg', upload_to = 'media/')
-    projects = models.ForeignKey(Projects, on_delete= models.CASCADE, null = True)
-    contact = models.CharField(max_length=250, null = True)
-    
-    def __str__(self):
-        return self.username
-    
-    def save_profile(self):
-        self.save()
-        
+            
    
 class Review(models.Model):
     '''
     Classes:
         design, usability, content
     '''
+    review_tup = [(1, '1'),(1, '1') ,(2, '2'),(3, '3'), (4, '4'),( 5, '5') ,(6, '6'), (7, '7'), (8, '8'), (9, '9')]
+    design_rate = models.IntegerField(choices = review_tup, null = True, default = 1)
+    usability_rate = models.IntegerField(choices = review_tup, null = True, default = 1)
+    content_rate = models.IntegerField(choices = review_tup, default = 1)
+    project = models.OneToOneField(Projects, on_delete= models.CASCADE, null = True, default = 1)
     
     # design review
-    class Design(models.IntegerChoices):
-        Exempolary = 4
-        Good = 3
-        Average = 2
-        Poor = 1
-    design_rate = models.IntegerField(choices=Design.choices)
+    # class Design(models.IntegerChoices):
+    #     Exempolary = 4
+    #     Good = 3
+    #     Average = 2
+    #     Poor = 1
+    
     
     # usability review    
-    class Usability(models.IntegerChoices):
-        Exempolary = 4
-        Good = 3
-        Average = 2
-        Poor = 1
-    usability_rate = models.IntegerField(choices=Usability.choices)
+    # class Usability(models.IntegerChoices):
+    #     Exempolary = 4
+    #     Good = 3
+    #     Average = 2
+    #     Poor = 1
+    # usability_rate = models.IntegerField(choices=Usability.choices)
     
-    # content review
-    class Content(models.IntegerChoices):
-        Exempolary = 4
-        Good = 3
-        Average = 2
-        Poor = 1
-    content_rate = models.IntegerField(choices=Content.choices)
+    # # content review
+    # class Content(models.IntegerChoices):
+    #     Exempolary = 4
+    #     Good = 3
+    #     Average = 2
+    #     Poor = 1
+    # content_rate = models.IntegerField(choices=Content.choices)
     
